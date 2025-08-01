4e7f33026bf8706ec0e98219a8d1891b415f7b27
screwthecap1 <chanskiy777@mail.ru>
2025-08-01 19:45:02 +0400

    Add example config file

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'dbname' => 'your_database_name',
+    'charset' => 'utf8mb4',
+    'username' => 'your_username',
+    'password' => 'your_password',
+];
